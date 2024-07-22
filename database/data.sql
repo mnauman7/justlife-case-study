a49e57f9706bd7c980ffedd2216d804545fdf38c
Mohd Nauman <nauman.exo@gmail.com>
2024-07-22 14:56:52 +0500

    updated scheme and data

--- database/data.sql
+++ database/data.sql
@@ -1,4 +1,48 @@
 -- Setting up a test user
-INSERT INTO app_db.`user`
-(first_name, last_name, address, city, telephone, is_admin, is_active, email, password)
-VALUES('Nauman', 'Tester', 'test town', 'lahore', '1212121212', 1, 1, 'nauman.tester@test.com', 'testing14');
+INSERT INTO app_db.`Users`
+(first_name, last_name, address, city, phone, is_admin, is_active, email, password)
+VALUES('Nauman', 'Tester', 'house 15, model town', 'lahore', '00923334445556', 1, 1, 'nauman.@test.com', 'testing12');
+
+-- Setting up sample vehicles
+INSERT INTO app_db.`Vehicles`
+(vehicle_id, vehicle_name, capacity, phone)
+VALUES(1, 'Van 1', 5, '00923334445557'),
+(2, 'Van 2', 5, '00923334445558'),
+(3, 'Van 3', 5, '00923334445559'),
+(4, 'Van 4', 5, '00923334445560'),
+(5, 'Van 5', 5, '00923334445561');
+
+-- Setting up job types
+INSERT INTO app_db.`JobTypeMaster`
+(job_type_id, job_type_name)
+VALUES(1, 'Cleaning'),(2, 'Spa');
+
+-- Setting up staff
+INSERT INTO app_db.`Staff`
+(first_name, last_name, phone, job_type_id, vehicle_id)
+VALUES
+('Sara', 'Oki', '00923334445562', 1, 1), -- vehicle 1 staff
+('Ali', 'Khan', '00923334445563', 1, 1),
+('John', 'Adam', '00923334445564', 1, 1),
+('Amna', 'Al', '00923334445565', 1, 1),
+('Olive', 'Marsh', '00923334445566', 1, 1),
+('Bo', 'Jensen', '00923334445567', 1, 2), -- vehicle 2 staff
+('Nora', 'Glass', '00923334445568', 1, 2),
+('Amin', 'Fahim', '000923334445569', 1, 2),
+('Tayyab', 'Khan', '00923334445570', 1, 2),
+('Jack', 'Jo', '00923334445571', 1, 2),
+('Alan', 'Bass', '00923334445572', 1, 3), -- vehicle 3 staff
+('Sky', 'Chen', '00923334445573', 1, 3),
+('Jackie', 'Chan', '00923334445574', 1, 3),
+('Shahrukh', 'Khan', '00923334445575', 1, 3),
+('Malay', 'Ray', '00923334445576', 1, 3),
+('Amna', 'Ali', '00923334445577', 1, 4), -- vehicle 4 staff
+('Creed', 'Chan', '00923334445578', 1, 4),
+('Walter', 'Vu', '00923334445579', 1, 4),
+('Jessie', 'Beck', '00923334445580', 1, 4),
+('Umar', 'Farooq', '00923334445581', 1, 4),
+('Atif', 'Ejaz', '00923334445582', 1, 5), -- vehicle 5 staff
+('Iqra', 'Zafar', '00923334445583', 1, 5),
+('Adnan', 'Khan', '00923334445584', 1, 5),
+('Mack', 'Ayala', '00923334445585', 1, 5),
+('Kole', 'Liu', '00923334445586', 1, 5);
--- database/schema.sql
+++ database/schema.sql
@@ -6,16 +6,42 @@ GRANT ALL PRIVILEGES ON app_db.* TO 'app_server'@'%' WITH GRANT OPTION;
 
 USE app_db;
 
-CREATE TABLE IF NOT EXISTS user (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Users (
+  user_id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   address VARCHAR(255),
   city VARCHAR(80),
-  telephone VARCHAR(20),
+  phone VARCHAR(20),
   is_admin BOOL NOT NULL DEFAULT 0, 
   is_active BOOL NOT NULL DEFAULT 1,
   email varchar(255) NOT NULL,
   password varchar(100) NOT NULL,
   INDEX(last_name)
+) engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Vehicles (
+  vehicle_id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  vehicle_name VARCHAR(30) NOT NULL,
+  capacity TINYINT UNSIGNED NOT NULL DEFAULT 5,
+  phone VARCHAR(20),
+  is_active BOOL NOT NULL DEFAULT 1
+) engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS JobTypeMaster (
+  job_type_id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  job_type_name VARCHAR(30) NOT NULL,
+  is_active BOOL NOT NULL DEFAULT 1
+) engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Staff (
+  staff_id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  phone VARCHAR(20),
+  job_type_id TINYINT UNSIGNED NOT NULL,
+  vehicle_id INT(4) UNSIGNED NOT NULL,
+  is_active BOOL NOT NULL DEFAULT 1,
+  FOREIGN KEY (job_type_id) REFERENCES JobTypeMaster(job_type_id),
+  FOREIGN KEY (vehicle_id) REFERENCES Vehicles(vehicle_id)
 ) engine=InnoDB;
